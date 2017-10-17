4a26b940691099c89eb35184a9c25be244f5a14d
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-10-17 13:23:53 -0600

    Made the base forms of the ChatRunner, ChatController, and PopupDisplay classes.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,6 +1,21 @@
 package chat.controller;
 
+import chat.view.PopupDisplay;
+//import chat.model.Chatbot;
+
 public class ChatController
 {
-
+	//Chatbot chatbot;
+	PopupDisplay display;
+	
+	public ChatController()
+	{
+		//chatbot = new Chatbot();
+		display = new PopupDisplay();
+	}
+	
+	public void start()
+	{
+		
+	}
 }
--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,6 +1,17 @@
 package chat.view;
 
+import javax.swing.JOptionPane;
+
 public class PopupDisplay
 {
-
+	public void displayMessage(String messageToDisplay)
+	{
+		JOptionPane.showMessageDialog(null, messageToDisplay);
+	}
+	
+	public String getResponse(String prompt)
+	{
+		String answer = JOptionPane.showInputDialog(null, prompt);
+		return answer;
+	}
 }
