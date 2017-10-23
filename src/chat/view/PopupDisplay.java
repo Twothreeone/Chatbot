463bc7f4126b76605b85a2a81493542b5fe24144
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-10-23 11:55:46 -0600

    Added an image to be used by PopupDisplay. Added icon and windowTitle data members to PopupDisplay, modified its methods and added a constructor.

--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,17 +1,27 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class PopupDisplay
 {
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("images/Triforce.png"));
+		windowTitle = "Chatbot says";
+	}
 	public void displayMessage(String messageToDisplay)
 	{
-		JOptionPane.showMessageDialog(null, messageToDisplay);
+		JOptionPane.showMessageDialog(null, messageToDisplay, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	
 	public String getResponse(String prompt)
 	{
-		String answer = JOptionPane.showInputDialog(null, prompt);
+		String answer = "";
+		answer += JOptionPane.showInputDialog(null, prompt, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
 		return answer;
 	}
 }
