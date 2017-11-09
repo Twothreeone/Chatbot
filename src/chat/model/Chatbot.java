cf4942bf0c3381618c3b8cb35d6899353576d40d
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-11-09 13:54:08 -0700

    Added some java doc stuff and made some of the methods less bulky.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -3,28 +3,27 @@ package chat.model;
 import java.util.List;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Chatbot
 {
 	private List<Movie> movieList;
 	private List<String> shoppingList;
 	private List<String> cuteAnimalMemes;
-	private String [] verbs;
-	private String [] topics;
-	private String [] followUps;
-	private String [] questions;
+	private String[] verbs;
+	private String[] topics;
+	private String[] followUps;
+	private String[] questions;
 	private String username;
 	private String content;
 	private String intro;
 	private LocalTime currentTime;
-	
+
 	public Chatbot(String username)
 	{
 		this.movieList = new ArrayList<Movie>();
-		this.shoppingList = new ArrayList<String>();
 		this.cuteAnimalMemes = new ArrayList<String>();
 		this.currentTime = LocalTime.now();
-		this.questions = new String[10];
 		this.username = username;
 		this.content = "content";
 		this.intro = "";
@@ -35,10 +34,10 @@ public class Chatbot
 		buildShoppingList();
 		buildVerbs();
 		buildQuestions();
-//		buildFollowups();
+		// buildFollowups();
 		buildTopics();
 	}
-	
+
 	private void buildTopics()
 	{
 		topics[0] = "math";
@@ -47,7 +46,7 @@ public class Chatbot
 		topics[3] = "anime";
 		topics[4] = "work";
 	}
-	
+
 	private void buildVerbs()
 	{
 		verbs[0] = "like";
@@ -65,48 +64,49 @@ public class Chatbot
 		movieList.add(new Movie("Space Balls"));
 		movieList.add(new Movie("Monty Python and the Quest for the Holy Grail"));
 	}
-	
+
+	/**
+	 * Creates the shoppingList ArrayList that will be used for the chatbot's responses.
+	 */
 	private void buildShoppingList()
 	{
-		shoppingList.add("snacks");
-		shoppingList.add("veggies");
-		shoppingList.add("protein");
-		shoppingList.add("red cream soda");
-		shoppingList.add("ice cream");
-		shoppingList.add("potato chips");
-		shoppingList.add("pringles");
-		shoppingList.add("candy");
-		shoppingList.add("donuts");
-		shoppingList.add("cake");
-		shoppingList.add("gum");
+		shoppingList = new ArrayList<String>(Arrays.asList("snacks", "veggies", "protein", "red cream soda", "ice cream", "potato chips", "pringles", "candy", "donuts", "cake", "gum"));
 	}
-	
+
 	private void buildCuteAnimals()
 	{
-		
+
 	}
-	
+
+	/**
+	 * Creates the questions array that will be used for the chatbot's responses.
+	 */
 	private void buildQuestions()
 	{
-		questions[0] = "What is your name?";
-		questions[1] = "What are your favorite hobbies?";
-		questions[2] = "How do you live?";
-		questions[3] = "What is your favorite color?";
-		questions[4] = "Why?";
-		questions[5] = "What is your least favorite emotion?";
-		questions[6] = "What is your job?";
-		questions[7] = "Why are you so useless?";
-		questions[8] = "Do you sleep?";
-		questions[9] = "What is your favorite movie?";
+		questions = new String[] { "What is your name?", "What are your favorite hobbies?", "How do you live?", "What is your favorite color?", "Why?", "What is your least favorite emotion?",
+				"What is your job?", "Why are you so useless?", "Do you sleep?", "What is your favorite movie?" };
 	}
-	
+
+	/**
+	 * Takes in the users input and runs buildChatbotResponse() to craft what the chatbot will respond
+	 * with.
+	 * 
+	 * @param input
+	 *            What the user said to the chatbot.
+	 * @return What the chatbot will say to the user.
+	 */
 	public String processConversation(String input)
 	{
 		String chatbotResponse = "You said:" + "\n" + input + "\n";
 		chatbotResponse += buildChatbotResponse();
 		return chatbotResponse;
 	}
-	
+
+	/**
+	 * Randomly creates what the chatbot will say back to the user using arrays and Math.random().
+	 * 
+	 * @return The String the chatbot will respond with.
+	 */
 	private String buildChatbotResponse()
 	{
 		String response = "I ";
@@ -124,7 +124,7 @@ public class Chatbot
 		}
 		return response;
 	}
-	
+
 	public boolean lengthChecker(String input)
 	{
 		boolean properLength = false;
@@ -134,25 +134,25 @@ public class Chatbot
 		}
 		return properLength;
 	}
-	
+
 	public boolean htmlTagChecker(String input)
 	{
 		return false;
 	}
-	
+
 	public boolean userNameChecker(String input)
 	{
 		if (input == null)
 		{
 			return false;
 		}
-		if (input.length() < 2 || input.charAt(0) != '@' || input.substring(1,input.length()).contains("@"))
+		if (input.length() < 2 || input.charAt(0) != '@' || input.substring(1, input.length()).contains("@"))
 		{
 			return false;
 		}
 		return true;
 	}
-	
+
 	public boolean contentChecker(String contentCheck)
 	{
 		if (content.length() < 6)
@@ -165,12 +165,12 @@ public class Chatbot
 		}
 		return false;
 	}
-	
+
 	public boolean cuteAnimalMemeChecker(String input)
 	{
 		return false;
 	}
-	
+
 	public boolean shoppingListChecker(String shoppingItem)
 	{
 		for (String item : shoppingList)
@@ -182,12 +182,12 @@ public class Chatbot
 		}
 		return false;
 	}
-	
+
 	public boolean movieTitleChecker(String title)
 	{
 		return false;
 	}
-	
+
 	public boolean movieGenreChecker(String genre)
 	{
 		return false;
@@ -208,7 +208,7 @@ public class Chatbot
 
 	public boolean keyboardMashChecker(String sample)
 	{
-		String[] mash = {"sdf", "SDF", "dfg", "cvb", ",./", "kjh", "DFG", "CVB", "KJH"};
+		String[] mash = { "sdf", "SDF", "dfg", "cvb", ",./", "kjh", "DFG", "CVB", "KJH" };
 		for (String letters : mash)
 		{
 			if (sample.contains(letters))
@@ -218,32 +218,32 @@ public class Chatbot
 		}
 		return false;
 	}
-	
+
 	public String toString()
 	{
 		return "";
 	}
-	
+
 	public List<Movie> getMovieList()
 	{
 		return movieList;
 	}
-	
+
 	public List<String> getShoppingList()
 	{
 		return shoppingList;
 	}
-	
+
 	public List<String> getCuteAnimalMemes()
 	{
 		return cuteAnimalMemes;
 	}
 
-	public String [] getQuestions()
+	public String[] getQuestions()
 	{
 		return questions;
 	}
-	
+
 	public String[] getVerbs()
 	{
 		return verbs;
@@ -263,7 +263,7 @@ public class Chatbot
 	{
 		return username;
 	}
-	
+
 	public String getContent()
 	{
 		return content;
@@ -273,17 +273,17 @@ public class Chatbot
 	{
 		return intro;
 	}
-	
+
 	public LocalTime getCurrentTime()
 	{
 		return currentTime;
 	}
-	
+
 	public void setUsername(String username)
 	{
 		this.username = username;
 	}
-	
+
 	public void setContent(String content)
 	{
 		this.content = content;
