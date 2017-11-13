1866cc6194bb605a5dc358bc1a6689ad06385bd5
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-11-13 13:45:07 -0700

    Setup framework for ChatFrame and ChatPanel classes.

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,32 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
+{
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
