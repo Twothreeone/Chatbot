304af3a6dfa0b7b6a63578efee0c5768fcdfd879
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-11-15 13:09:01 -0700

    Made the setupLayout() method.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -37,7 +37,16 @@ public class ChatPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		appLayout.putConstraint(SpringLayout.NORTH, chatArea, 20, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, chatArea, 25, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatArea, -25, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.EAST, inputField, 0, SpringLayout.WEST, chatButton);
+		appLayout.putConstraint(SpringLayout.NORTH, chatButton, 2, SpringLayout.NORTH, inputField);
+		appLayout.putConstraint(SpringLayout.EAST, chatButton, 0, SpringLayout.EAST, chatArea);
+		appLayout.putConstraint(SpringLayout.SOUTH, chatArea, -60, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.NORTH, inputField, 10, SpringLayout.SOUTH, chatArea);
+		appLayout.putConstraint(SpringLayout.WEST, inputField, 25, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, inputField, -20, SpringLayout.SOUTH, this);
 	}
 	
 	private void setupListeners()
