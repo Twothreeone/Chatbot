242e282be89eeba0e8d03d1dec488b5b3a5259e1
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-11-15 12:40:32 -0700

    Added some imports and data members to ChatPanel.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -7,9 +7,17 @@ import javax.swing.JButton;
 import javax.swing.SpringLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class ChatPanel extends JPanel
 {
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
 	public ChatPanel(ChatbotController appController)
 	{
 		super();
