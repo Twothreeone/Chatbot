4d1250d7919d7cc41015137ebc3d396aee05fa47
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-27 11:52:36 -0700

    Added a txt file of the common words on twitter. Added an IOController and finished the loadFromFile method.

---
+++ src/chat/controller/IOController.java
@@ -0,0 +1,38 @@
+package chat.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+public class IOController
+{
+	public static void saveToFile(ChatbotController app, String textToSave, String path)
+	{
+		
+	}
+	
+	public static String loadFromFile(ChatbotController app, String filename)
+	{
+		String results = "";
+		try
+		{
+			File openFile = new File(filename);
+			Scanner fileScanner = new Scanner(openFile);
+			String currentLine = fileScanner.nextLine();
+			while(fileScanner.hasNextLine())
+			{
+				results += currentLine + "\n";
+				currentLine = fileScanner.nextLine();
+			}
+			results += currentLine + "\n";
+			fileScanner.close();
+		}
+		catch (IOException error)
+		{
+			app.handleErrors(error);
+		}
+		return results;
+	}
+}
