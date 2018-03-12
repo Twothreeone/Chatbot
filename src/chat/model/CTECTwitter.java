0f597a0ca228d8f9f83b85a8cc8683b02b918f13
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-12 13:28:22 -0600

    Finished the getMostCommonWord() method, still need to add the sortHashMap method.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -13,6 +13,7 @@ import twitter4j.QueryResult;
 import twitter4j.ResponseList;
 import java.util.Scanner;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.text.DecimalFormat;
@@ -62,6 +63,13 @@ public class CTECTwitter
 		trimTheBoringWords(boring);
 		removeBlanks();
 		generateWordCount();
+		ArrayList<Map.Entry<String, Integer>> sorted = sortHashMap();
+		String mostCommonWord = sorted.get(0).getKey();
+		int maxWord = 0;
+		maxWord = sorted.get(0).getValue();
+		mostCommon = "The most common word in " + username + "'s " + searchedTweets.size() + " tweets is " + mostCommonWord + ", and it was used " + maxWord + " times.\nThis is "
+				+ (DecimalFormat.getPercentInstance().format(((double) maxWord) / totalWordCount)) + " of total words: " + totalWordCount + " and is "
+				+ (DecimalFormat.getPercentInstance().format(((double) maxWord) / wordsAndCount.size())) + " of the unique words: " + wordsAndCount.size();
 		return mostCommon;
 	}
 
@@ -159,7 +167,7 @@ public class CTECTwitter
 			}
 		}
 	}
-	
+
 	private void removeBlanks()
 	{
 		for (int i = tweetedWords.size() - 1; i >= 0; i--)
@@ -170,7 +178,7 @@ public class CTECTwitter
 			}
 		}
 	}
-	
+
 	private void generateWordCount()
 	{
 		for (String word : tweetedWords)
